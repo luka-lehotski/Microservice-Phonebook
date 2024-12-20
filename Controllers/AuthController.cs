8d13182eb5b8cfb038e3bf512ed5b264a1cc8e3d
luka-lehotski <74432380+luka-lehotski@users.noreply.github.com>
2024-12-20 15:23:38 +0100

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using IdentityProviderMicroservice.Services;
+using IdentityProviderMicroservice.User;
+using IdentityProviderMicroservice;
+
+namespace IdentityProvider.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IIdentityProviderService _identityProviderService;
+
+        public AuthController(IIdentityProviderService identityProviderService)
+        {
+            _identityProviderService = identityProviderService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
+        {
+            AuthResult result = await _identityProviderService.VerifyUserPassword(loginRequest.Email, loginRequest.Password);
+
+            if (result.Status=="Success")
+            { 
+                // Generate JWT token or handle successful login
+                return Ok(new { Token = result.Jwt });
+            }
+            if(result.Status == "Wrong password")
+            {
+                return Unauthorized(new { message = "Invalid password." });
+            }
+            if (result.Status == "User doesn't exist")
+            {
+                return Unauthorized(new { message = result.Status });
+            }
+            return BadRequest();
+        }
+    }
+}
+
